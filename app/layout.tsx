41fff50628c31ec81ce4142a550f39f11998da64
Neha <neha_h@berkeley.edu>
2025-09-27 21:46:34 -0700

    set up styled components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import StyledComponentsRegistry from "@/lib/registry";
 import "@/styles/global.css";
 
 // font definitions
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={sans.className}>{children}</body>
+      <body className={sans.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
--- setup-scripts/_/styled-components/styles/colors.ts
+++
@@ -1,7 +0,0 @@
-const COLORS = {
-  // Add colors here as needed
-  black: '#000000',
-  white: '#FBFBFB',
-};
-
-export default COLORS;

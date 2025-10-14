fe5160ad4fc7f95c5153342837cc5d52703542c2
Justin Lee <91563628+jjstnlee@users.noreply.github.com>
2025-10-14 14:15:33 -0700

    [feat] Fetch Modules From Canvas For Supabase (#5)
    * created function to fetch lessons and update supabase
    
    * did work
    
    * [feat] Fetch Courses From Canvas For Supabase (#7)
    
    * Build script for fetching courses and updating database (WIP)
    
    * Split course syncing functionality into three files
    
    * Set teacher_id column to account_id
    
    * Map course_code property from API to join_code column in Groups
    
    * Update according to new schema; rename to match with other PRs
    
    ---------
    
    Co-authored-by: Nathan Tam <ntam1412@gmail.com>
    
    * updated function comments and updated schema
    
    * created function to fetch lessons and update supabase
    
    rebasing with main
    
    * updated function comments and updated schema
    
    rebasing with main
    
    * added comments to schema
    
    * Revert "added comments to schema"
    
    This reverts commit 402ae238b8f7a247895a704b4efe13f82c5fe6a1.
    
    reverting
    
    * rebasing
    
    ---------
    
    Co-authored-by: philipye314 <philipye314@gmail.com>
    Co-authored-by: Nathan <135177477+nathandtam@users.noreply.github.com>
    Co-authored-by: Nathan Tam <ntam1412@gmail.com>

--- api/supabase/queries/query.ts
+++ api/supabase/queries/query.ts
@@ -1,4 +1,5 @@
-import { fetchAllCourses } from "@/api/canvas/queries/query";
+import { fetchAllCourses, fetchAllModules } from "@/api/canvas/queries/query";
+import { CanvasModule, Lesson } from "@/types/schema";
 import supabase from "../client";
 
 /**
@@ -15,5 +16,25 @@ export async function updateCourses() {
 
   if (error) {
     console.error("Upsert error:", error);
+  }
+}
+
+/**
+ * Fetches modules from Canvas and updates the Lessons table in Supabase.
+ * @param groupId - Canvas course ID
+ */
+export async function updateLessons(groupId: number) {
+  const modules = await fetchAllModules(groupId);
+
+  const lessons = modules.map((module: CanvasModule) => ({
+    id: module.id,
+    name: module.name,
+    group_id: groupId,
+  })) as Lesson[];
+
+  const { error } = await supabase.from("Lessons").insert(lessons);
+
+  if (error) {
+    throw new Error(`Error inserting data: ${error.message}`);
   }
 }
--- types/schema.d.ts
+++ types/schema.d.ts
@@ -8,10 +8,27 @@ export type CanvasCourse = {
 };
 
 /**
- * A Canvas course for the Groupts table.
+ * A Canvas course for the Groups table.
  */
 export type Group = {
   id: number;
   join_code: string;
   name: string;
+};
+
+/**
+ * A Canvas module for the Lessons table.
+ */
+export type CanvasModule = {
+  id: number;
+  name: string;
+};
+
+/**
+ * A Lesson for the Lessons table.
+ */
+export type Lesson = {
+  id: number;
+  name: string;
+  group_id: number;
 };

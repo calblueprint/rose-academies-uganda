ad154de80077963c85676b7141f9c41e1c5f98dc
Justin Lee <jjunseol04@gmail.com>
2025-10-23 20:54:08 -0700

    created sync button with functionality (moving data from supabase to sqlite)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import React, { CSSProperties } from "react";
 import LessonPage from "../components/LessonPage";
 
 export default function Home() {
+  const handleSync = async () => {
+    try {
+      const res = await fetch("/api/sync");
+      const data = await res.json();
+    } catch (err) {
+      console.error("Error syncing data:", err);
+    }
+  };
+
   return (
     <main style={mainStyles}>
       <LessonPage />
+      <button onClick={handleSync}>Sync</button>
     </main>
   );
 }

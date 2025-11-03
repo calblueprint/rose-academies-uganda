2d5d3973ef4ee6bcbfd9e18aa5fe9f9422997fbe
Nathan Tam <ntam1412@gmail.com>
2025-11-03 14:30:25 -0800

    Test file preview (wip)

---
+++ components/FilePreview/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { LocalFile } from "@/types/schema";
+
+export function FilePreview({ file }: { file: LocalFile }) {
+  if (!file) return <p>No file selected.</p>;
+  return (
+    <iframe
+      src={`/api/sqlite/files/${file.id}`}
+      className="w-full h-[80vh] border"
+      title={file.name}
+    />
+  );
+}

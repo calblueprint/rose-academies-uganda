01c4b5c2ea6d562881d7d38c01cb8564d5840a73
Emilypeng1104 <zishan.e.peng@gmail.com>
2025-11-06 00:49:40 -0800

    [fix] header style px to rem

--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -4,27 +4,25 @@ import styled from "styled-components";
 
 export const HeaderBar = styled.header`
   display: flex;
-  width: 1440px;
-  height: 84px;
-  padding: 13px 116px;
+  width: 90rem;
+  height: 5.25rem;
+  padding: 0.8125rem 7.25rem;
   justify-content: space-between;
   align-items: center;
   flex-shrink: 0;
   background: var(--Surface-Light, #fff);
-
-  /* 4DP Elevation */
-  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.06);
+  box-shadow: 0 0.125rem 0.5rem 0 rgba(0, 0, 0, 0.06);
 `;
 
 export const Left = styled.div`
   display: flex;
   align-items: center;
-  gap: 7px;
+  gap: 0.4375rem;
 `;
 
 export const Logo = styled.img`
-  width: 45px;
-  height: 45px;
+  width: 2.8125rem;
+  height: 2.8125rem;
   aspect-ratio: 1/1;
   background: url(<path-to-image>) lightgray 50% / cover no-repeat;
 `;
@@ -32,10 +30,10 @@ export const Logo = styled.img`
 export const TeamName = styled.h1`
   color: #000;
   font-family: Gilroy-Medium;
-  font-size: 20px;
+  font-size: 1.25rem;
   font-style: normal;
   font-weight: 400;
-  line-height: 23.343px; /* 116.716% */
+  line-height: 1.459rem;
 `;
 
 export const Right = styled.div`

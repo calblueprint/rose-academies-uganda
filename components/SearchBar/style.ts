7c3f965a4439edec0c52891ad61b816d240b2dda
Emilypeng1104 <zishan.e.peng@gmail.com>
2025-11-06 16:16:22 -0800

    [fix] searchbar px to rem

--- components/SearchBar/style.ts
+++ components/SearchBar/style.ts
@@ -4,30 +4,30 @@ export const SearchBarContainer = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-  gap: 21px;
+  gap: 1.3125rem;
   align-self: stretch;
 `;
 
 export const SearchBarField = styled.div`
   display: flex;
-  width: 907px;
-  height: 50px;
-  padding: 5px 24px;
+  width: 56.6875rem;
+  height: 3.125rem;
+  padding: 0.3125rem 1.5rem;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  gap: 10px;
-  border-radius: 20px;
-  border-right: 1px solid #d9d9d9;
-  border-bottom: 1px solid #d9d9d9;
-  border-left: 1px solid #d9d9d9;
+  gap: 0.625rem;
+  border-radius: 1.25rem;
+  border-right: 0.0625rem solid #d9d9d9;
+  border-bottom: 0.0625rem solid #d9d9d9;
+  border-left: 0.0625rem solid #d9d9d9;
   background: #fafafa;
 `;
 
 export const SearchButton = styled.image`
-  width: 20px;
-  height: 20px;
-  aspect-ratio: 1/1;
+  width: 1.25rem;
+  height: 1.25rem;
+  aspect-ratio: 1 / 1;
   fill: #808582;
 `;
 
@@ -40,10 +40,10 @@ export const SearchInput = styled.input`
   color: #808582;
   text-overflow: ellipsis;
   font-family: Gilroy-Regular;
-  font-size: 18px;
+  font-size: 1.125rem;
   font-style: normal;
   font-weight: 400;
-  line-height: 18px; /* 100% */
+  line-height: 1.125rem;
 `;
 
 export const ClearButton = styled.button`
@@ -52,9 +52,9 @@ export const ClearButton = styled.button`
   align-items: center;
   justify-content: center;
   line-height: 0;
-  width: 12px;
-  height: 12px;
-  aspect-ratio: 1/1;
+  width: 0.75rem;
+  height: 0.75rem;
+  aspect-ratio: 1 / 1;
   fill: var(--Gray, #808582);
   margin-left: auto;
 `;

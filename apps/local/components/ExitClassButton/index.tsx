27b23cbfcbf52ed96a3bfc9d06ba2e430b3595d4
Nathan Tam <ntam1412@gmail.com>
2026-03-04 13:06:27 -0800

    make exit button dependent on page (sync or lessons)

--- apps/local/components/ExitClassButton/index.tsx
+++ apps/local/components/ExitClassButton/index.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import React from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import ActionButton from "@/components/ActionButton";
 import COLORS from "@/styles/colors";
 
 export default function ExitClassButton() {
   const router = useRouter();
+  const pathname = usePathname();
+
+  let buttonText = "Exit Class";
+
+  if (pathname.startsWith("/sync")) {
+    buttonText = "View Lessons";
+  }
 
   return (
     <ActionButton
@@ -15,7 +22,7 @@ export default function ExitClassButton() {
       textColor={COLORS.white}
       iconColor={COLORS.white}
       iconType={"exitClass"}
-      text={"Exit Class"}
+      text={buttonText}
       title="Click to refresh status"
       animationDuration="1s"
       borderColor="#e5e7eb"

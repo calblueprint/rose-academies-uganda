cd6f6031e87c02118045e5e3c95373d524a94997
Justin Fu <jfu7171@berkeley.edu>
2026-03-14 12:39:03 -0700

    [wip] Added toggle functionality as well as adding a button to the lessons grid

--- apps/cloud/app/(authed)/app/lessons/page.tsx
+++ apps/cloud/app/(authed)/app/lessons/page.tsx
@@ -5,9 +5,10 @@ export default async function LessonsPage() {
   const supabase = await getSupabaseServerClientReadOnly();
 
   const { data: lessons, error } = await supabase
-    .from("Lessons")
-    .select("id, name, description, image_path, group_id")
-    .order("id", { ascending: true });
+    .from("Lessons_copy")
+    .select("id, name, description, image_path, group_id, is_archived")
+    .order("id", { ascending: true })
+    .eq("is_archived", false);
 
   if (error) {
     throw new Error(error.message);

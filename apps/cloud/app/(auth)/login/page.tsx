73281ff34f3d067efdcb84f2c6273398b5d62bd8
pranavsant <pranavsant@berkeley.edu>
2026-03-30 02:25:55 -0700

    cleaned up LoginPage component and added StyledComponentsRegistry in layout file

--- apps/cloud/app/(auth)/login/page.tsx
+++ apps/cloud/app/(auth)/login/page.tsx
@@ -1,9 +1,5 @@
 import LoginForm from "@/app/(auth)/components/LoginForm";
 
 export default function LoginPage() {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />;
 }

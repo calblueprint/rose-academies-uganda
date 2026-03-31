f9128271abb05f4a58153e36b7c88694eda8ea9e
Pranav Sant <pranavsant@berkeley.edu>
2026-03-31 12:45:37 -0700

    invalid login message functionality (#110)
    * restructured login folder
    
    * added state, error message, greyed out submit button that says logging in when button clicked
    
    * made LoginForm component into own folder
    
    * stop submit button from jumping when information incorrect
    
    * increased size of input, made cursor look like click pointer
    
    * cleaned up LoginPage component and added StyledComponentsRegistry in layout file

--- apps/cloud/app/(auth)/login/page.tsx
+++ apps/cloud/app/(auth)/login/page.tsx
@@ -1,55 +1,5 @@
-import { loginWithEmailPassword } from "@/actions/login";
-import * as S from "./style";
+import LoginForm from "@/app/(auth)/components/LoginForm";
 
 export default function LoginPage() {
-  async function loginAction(formData: FormData) {
-    "use server";
-
-    const email = String(formData.get("email") || "");
-    const password = String(formData.get("password") || "");
-
-    await loginWithEmailPassword({ email, password });
-  }
-
-  return (
-    <S.Main>
-      <S.Card>
-        <S.LogoWrap>
-          <S.Logo src="/logo.png" alt="Logo" />
-        </S.LogoWrap>
-
-        <S.H1>Login</S.H1>
-
-        <S.Form action={loginAction}>
-          <S.Label>
-            Email
-            <S.Input
-              name="email"
-              type="email"
-              required
-              autoComplete="email"
-              placeholder="Email"
-            />
-          </S.Label>
-
-          <S.Label>
-            Password
-            <S.Input
-              name="password"
-              type="password"
-              required
-              autoComplete="current-password"
-              placeholder="Password"
-            />
-          </S.Label>
-
-          <S.Button type="submit">Log in</S.Button>
-        </S.Form>
-
-        <S.Paragraph>
-          Invite-only. Contact an administrator for access.
-        </S.Paragraph>
-      </S.Card>
-    </S.Main>
-  );
+  return <LoginForm />;
 }

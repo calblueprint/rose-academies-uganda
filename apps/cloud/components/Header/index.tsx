f5d85609ef99f427828316ab92d1a9ed414a447e
Pranav Sant <pranavsant@berkeley.edu>
2026-04-06 00:56:19 -0700

    Added displayname to Header to remove hardcoded name (#145)
    * added displayname to Header to remove hardcoded name, get display name in layout to pass into header
    
    * made intials better for more cases
    
    * add user fallback as backup
    
    ---------
    
    Co-authored-by: Nathan Tam <ntam1412@gmail.com>

--- apps/cloud/components/Header/index.tsx
+++ apps/cloud/components/Header/index.tsx
@@ -24,7 +24,7 @@ import {
   UserName,
 } from "./styles";
 
-export default function Header() {
+export default function Header({ displayName }: { displayName: string }) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const dropdownRef = useRef<HTMLDivElement>(null);
@@ -44,6 +44,15 @@ export default function Header() {
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
+
+  const initials =
+    displayName
+      .trim()
+      .match(/[\p{L}''\-]+/gu)
+      ?.filter((_, i, arr) => i === 0 || i === arr.length - 1)
+      .map(word => word[0])
+      .join("")
+      .toUpperCase() ?? "";
 
   return (
     <HeaderBar>
@@ -78,8 +87,8 @@ export default function Header() {
       <HeaderRight>
         <DropdownWrapper ref={dropdownRef}>
           <ProfileButton onClick={() => setDropdownOpen(prev => !prev)}>
-            <UserImg>NH</UserImg>
-            <UserName>Neha Hussain</UserName>
+            <UserImg>{initials}</UserImg>
+            <UserName>{displayName}</UserName>
           </ProfileButton>
 
           {dropdownOpen && (

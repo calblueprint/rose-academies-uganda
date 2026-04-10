a54341540aa01ee7077f73140f0436dc38a47562
Nathan Huynh <124105634+nathanhuynhers@users.noreply.github.com>
2026-04-09 23:43:09 -0700

    Nathanh/update our sync file to sync from offline library and use teacherpi relationships (#153)
    * testing sync
    
    * pretty sure this works

--- apps/cloud/lib/getCurrentUserDevice.ts
+++ apps/cloud/lib/getCurrentUserDevice.ts
@@ -1,4 +1,31 @@
-export function getCurrentDeviceId() {
-  return "nathans-pi";
-  // temporary, edit later
+import { getSupabaseServerClientReadOnly } from "@/api/supabase/server-readonly";
+
+type GetCurrentDeviceIdOptions = {
+  userId?: string;
+};
+
+export async function getCurrentDeviceId(
+  options: GetCurrentDeviceIdOptions = {},
+) {
+  const supabase = await getSupabaseServerClientReadOnly();
+  const { userId: providedUserId } = options;
+
+  const userId =
+    providedUserId ?? (await supabase.auth.getUser()).data.user?.id;
+
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+
+  const { data: device, error } = await supabase
+    .from("devices")
+    .select("id")
+    .eq("user_id", userId)
+    .single();
+
+  if (error || !device?.id) {
+    throw error ?? new Error("Unable to find device for user.");
+  }
+
+  return device.id as string;
 }

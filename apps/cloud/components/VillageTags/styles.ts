3ecb63f2859be93f554d15d3dabbc6d60fe4170e
pranavsant <pranavsant@berkeley.edu>
2026-04-14 03:49:13 -0700

    fixed tag styling

--- apps/cloud/components/VillageTags/styles.ts
+++ apps/cloud/components/VillageTags/styles.ts
@@ -4,14 +4,12 @@ import COLORS from "@/styles/colors";
 export const VillageTag = styled.span`
   display: flex;
   padding: 0.25rem 0.75rem;
-  background: var(--green-20, #dee3d1);
 
+  background-color: #dee3d1;
+  border-radius: 999px;
+  padding: 6px 16px;
+  font-size: 14px;
   color: ${COLORS.evergreen};
-  font-family: "Google Sans";
-  font-size: 0.875rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
 `;
 
 export const TagGroup = styled.div`

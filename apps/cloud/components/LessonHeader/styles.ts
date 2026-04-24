278e21bce8cd5581b6850844b13d61486b02dce4
Nathan Tam <ntam1412@gmail.com>
2026-04-23 19:14:49 -0700

    fix LessonHeader

--- apps/cloud/components/LessonHeader/styles.ts
+++ apps/cloud/components/LessonHeader/styles.ts
@@ -1,25 +1,31 @@
 import Link from "next/link";
 import styled from "styled-components";
+import COLORS from "@/styles/colors";
 
 export const HeaderContainer = styled.div`
   width: 100%;
 `;
 
 export const ImageBanner = styled.div<{ $imagePath: string | null }>`
+  position: relative;
+
   width: 100%;
   height: 8.8125rem;
   border-radius: 0.625rem;
+
   background: ${({ $imagePath }) =>
     $imagePath
       ? `linear-gradient(rgba(30, 66, 64, 0.2), rgba(30, 66, 64, 0.2)), url(${$imagePath})`
-      : "var(--evergreen-100, #1e4240)"};
+      : COLORS.evergreen};
+
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+
   padding: 1.5rem;
+
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
 `;
 
 export const HeaderTopRow = styled.div`
@@ -32,34 +38,49 @@ export const BackLink = styled(Link)`
   display: inline-flex;
   align-items: center;
   gap: 0.75rem;
+
   padding: 0.5rem 0;
-  color: white;
+
+  color: ${COLORS.white};
   text-decoration: none;
-  font-size: 0.875rem;
+
+  font-size: var(--font-subtitle-2);
+  line-height: var(--lh-subtitle-2);
   font-weight: 500;
 `;
 
 export const CustomizeButton = styled.button`
+  position: absolute;
+  top: 0.75rem;
+  right: 0.75rem;
+
   display: inline-flex;
-  padding: 0.5rem 1rem;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
   gap: 0.5rem;
+
+  padding: 0.5rem 1rem;
   border-radius: 0.5rem;
   border: none;
+
   background: rgba(244, 245, 247, 0.7);
-  color: var(--evergreen-100, #1e4240);
-  font-size: 1rem;
+  color: ${COLORS.evergreen};
+
+  font-size: var(--font-subtitle-2);
+  line-height: var(--lh-subtitle-2);
   font-weight: 500;
+
   cursor: pointer;
 `;
 
 export const LessonTitle = styled.h1`
   margin: 1.5rem 0 0 0;
-  color: #191c1c;
-  font-size: 3rem;
+
+  color: ${COLORS.evergreen};
+
+  font-size: var(--font-h1);
+  line-height: var(--lh-h1);
   font-weight: 400;
-  line-height: normal;
 `;
 
 export const IconContainer = styled.span`

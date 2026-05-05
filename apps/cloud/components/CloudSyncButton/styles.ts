bc26a894644945e4cf422237f704c4d087dcaaed
Nathan Tam <ntam1412@gmail.com>
2026-05-04 17:39:00 -0700

    fix styling

--- apps/cloud/components/CloudSyncButton/styles.ts
+++ apps/cloud/components/CloudSyncButton/styles.ts
@@ -3,26 +3,32 @@ import COLORS from "@/styles/colors";
 import { Subtitle1, Subtitle2 } from "@/styles/text";
 
 export const SyncCard = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  gap: 24px;
-  padding: 24px;
-  border-radius: 16px;
+  display: grid;
+  width: 37.375rem;
+  height: 11.375rem;
+  grid-template-rows: auto 2.75rem 3.25rem;
+  gap: 0.5rem;
+  padding: 1.5rem;
+  border-radius: 1rem;
   background: ${COLORS.white};
-  box-shadow: 0 8px 24px rgb(0 0 0 / 12%);
+  box-shadow:
+    0 22px 6px 0 rgba(170, 170, 170, 0),
+    0 14px 6px 0 rgba(170, 170, 170, 0.01),
+    0 8px 5px 0 rgba(170, 170, 170, 0.05),
+    0 4px 4px 0 rgba(170, 170, 170, 0.09),
+    0 1px 2px 0 rgba(170, 170, 170, 0.1);
 `;
 
 export const CardTitle = styled(Subtitle2).attrs({
   $color: COLORS.gray100,
   $fontWeight: 500,
 })`
-  font-size: 16px;
+  font-size: 1rem;
 `;
 
 export const LastSyncedInfo = styled.div`
   display: flex;
-  height: 18px;
+  height: 2.75rem;
   justify-content: space-between;
   align-items: center;
   align-self: stretch;
@@ -31,13 +37,15 @@ export const LastSyncedInfo = styled.div`
 export const SyncProgressArea = styled.div`
   display: flex;
   width: 100%;
+  height: 2.75rem;
   flex-direction: column;
-  gap: 12px;
+  justify-content: center;
+  gap: 0.75rem;
 `;
 
 export const ProgressBar = styled.div`
   width: 100%;
-  height: 8px;
+  height: 0.5rem;
   border-radius: 999px;
   background: ${COLORS.gray40};
   overflow: hidden;
@@ -68,12 +76,12 @@ const spin = keyframes`
 
 export const ButtonWrapper = styled.button`
   display: flex;
+  width: 100%;
   height: 3.25rem;
   padding: 0.625rem;
   justify-content: center;
   align-items: center;
-  flex: 1 0 0;
-  gap: 8px;
+  gap: 0.5rem;
 
   border-radius: 0.75rem;
   background: ${COLORS.evergreen};
